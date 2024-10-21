453b6276cc9bb12cb9278ba5295d3b51055d563c
alejo <alejo@DESKTOP-MMQ2CAG>
2024-10-21 11:01:57 +0200

    V3

--- DAM1TRIMESTRE/src/Interfaces/Calculadora.java
+++ DAM1TRIMESTRE/src/Interfaces/Calculadora.java
@@ -7,7 +7,6 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JTextField;
-import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -48,7 +47,7 @@ public class Calculadora extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        // Create number buttons and operators
+        // Botones de números
         JButton bt1 = new JButton("1");
         bt1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -79,8 +78,6 @@ public class Calculadora extends JFrame {
         bt3.setBounds(279, 309, 109, 39);
         contentPane.add(bt3);
 
-        // Add more buttons...
-
         JButton btigual = new JButton("=");
         btigual.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -91,6 +88,7 @@ public class Calculadora extends JFrame {
         btigual.setBounds(470, 360, 109, 39);
         contentPane.add(btigual);
 
+        // Botones de operadores
         JButton btsum = new JButton("+");
         btsum.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -101,7 +99,35 @@ public class Calculadora extends JFrame {
         btsum.setBounds(470, 309, 109, 39);
         contentPane.add(btsum);
 
-        // Add more operator buttons (subtraction, multiplication, division)...
+        JButton btresta = new JButton("-");
+        btresta.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setOperator("-");
+            }
+        });
+        btresta.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        btresta.setBounds(470, 260, 109, 39);
+        contentPane.add(btresta);
+
+        JButton btmultiplica = new JButton("*");
+        btmultiplica.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setOperator("*");
+            }
+        });
+        btmultiplica.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        btmultiplica.setBounds(470, 211, 109, 39);
+        contentPane.add(btmultiplica);
+
+        JButton btdivide = new JButton("/");
+        btdivide.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setOperator("/");
+            }
+        });
+        btdivide.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        btdivide.setBounds(470, 162, 109, 39);
+        contentPane.add(btdivide);
 
         JButton btborrar = new JButton("BORRAR");
         btborrar.addActionListener(new ActionListener() {
@@ -120,11 +146,82 @@ public class Calculadora extends JFrame {
         textField.setBounds(41, 69, 345, 56);
         contentPane.add(textField);
         textField.setColumns(10);
+
+        // Más botones de números
+        JButton bt4 = new JButton("4");
+        bt4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("4");
+            }
+        });
+        bt4.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt4.setBounds(41, 260, 109, 39);
+        contentPane.add(bt4);
+
+        JButton bt5 = new JButton("5");
+        bt5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("5");
+            }
+        });
+        bt5.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt5.setBounds(160, 260, 109, 39);
+        contentPane.add(bt5);
+
+        JButton bt6 = new JButton("6");
+        bt6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("6");
+            }
+        });
+        bt6.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt6.setBounds(279, 260, 109, 39);
+        contentPane.add(bt6);
+
+        JButton bt7 = new JButton("7");
+        bt7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("7");
+            }
+        });
+        bt7.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt7.setBounds(41, 211, 109, 39);
+        contentPane.add(bt7);
+
+        JButton bt8 = new JButton("8");
+        bt8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("8");
+            }
+        });
+        bt8.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt8.setBounds(160, 211, 109, 39);
+        contentPane.add(bt8);
+
+        JButton bt9 = new JButton("9");
+        bt9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("9");
+            }
+        });
+        bt9.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt9.setBounds(279, 211, 109, 39);
+        contentPane.add(bt9);
+
+        JButton bt0 = new JButton("0");
+        bt0.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                appendNumber("0");
+            }
+        });
+        bt0.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        bt0.setBounds(160, 358, 109, 39);
+        contentPane.add(bt0);
     }
 
     private void appendNumber(String num) {
         String currentText = textField.getText();
-        textField.setText(currentText + num);  // Concatenar el número al texto existente
+        textField.setText(currentText + num);  
     }
 
     private void setOperator(String op) {
@@ -147,11 +244,16 @@ public class Calculadora extends JFrame {
             case "-":
                 result = firstOperand - secondOperand;
                 break;
-            case "X":
+            case "*":
                 result = firstOperand * secondOperand;
                 break;
-            case "%":
-                result = firstOperand / secondOperand;
+            case "/":
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                } else {
+                    textField.setText("Error: Div by 0");
+                    return;
+                }
                 break;
         }
 
